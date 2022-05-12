a506899d51b10d6afeaa8f3c0e74a12efc0f55a7
SameerKumar2002 <sameerkumar2002.zyd@gmail.com>
2022-05-12 16:09:58 +0630

    Initial commit

---
+++ app/src/main/java/com/sameer/firebaseregister/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.sameer.firebaseregister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var email: EditText
+    private lateinit var password : EditText
+    private lateinit var prograssBar : ProgressBar
+    private lateinit var register : Button
+    private lateinit var mAuth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+          email = findViewById(R.id.id_email)
+          password = findViewById(R.id.id_password)
+          register = findViewById(R.id.id_register)
+//          prograssBar = findViewById(R.id.id_prograss)
+
+          register.setOnClickListener {
+              SignUpHere()
+          }
+
+
+    }
+
+    private fun SignUpHere(){
+
+        val mEmail : String = email.text.toString()
+        val mPassword : String = password.text.toString()
+
+
+        mAuth = FirebaseAuth.getInstance()
+
+        Toast.makeText(applicationContext,mAuth.toString(),Toast.LENGTH_LONG).show()
+
+        mAuth.createUserWithEmailAndPassword(mEmail,mPassword)
+            .addOnCompleteListener {
+                if(it.isSuccessful){
+                    Toast.makeText(applicationContext,"Success",Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText( applicationContext,"Fail", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+    }
+}
